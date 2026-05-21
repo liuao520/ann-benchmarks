7a3a152c02725f1148763418f9d957d47beb5e2b
liuhao520 <1147958934@qq.com>
2026-05-21 17:23:58 +0800

    Improve ScaNN angular configuration

--- ann_benchmarks/algorithms/scann/module.py
+++ ann_benchmarks/algorithms/scann/module.py
@@ -5,7 +5,7 @@ from ..base.module import BaseANN
 
 
 class Scann(BaseANN):
-    def __init__(self, n_leaves, avq_threshold, dims_per_block, dist):
+    def __init__(self, n_leaves, avq_threshold, dims_per_block, dist, soar_lambda=None, overretrieve_factor=None):
         self.name = "scann n_leaves={} avq_threshold={:.02f} dims_per_block={}".format(
             n_leaves, avq_threshold, dims_per_block
         )
@@ -13,6 +13,8 @@ class Scann(BaseANN):
         self.avq_threshold = avq_threshold
         self.dims_per_block = dims_per_block
         self.dist = dist
+        self.soar_lambda = soar_lambda
+        self.overretrieve_factor = overretrieve_factor
 
     def fit(self, X):
         if self.dist == "dot_product":
@@ -24,7 +26,15 @@ class Scann(BaseANN):
 
         self.searcher = (
             scann.scann_ops_pybind.builder(X, 10, self.dist)
-            .tree(self.n_leaves, 1, training_sample_size=len(X), spherical=spherical, quantize_centroids=True)
+            .tree(
+                self.n_leaves,
+                1,
+                training_sample_size=len(X),
+                spherical=spherical,
+                quantize_centroids=True,
+                soar_lambda=self.soar_lambda,
+                overretrieve_factor=self.overretrieve_factor,
+            )
             .score_ah(self.dims_per_block, anisotropic_quantization_threshold=self.avq_threshold)
             .reorder(1)
             .build()
@@ -34,4 +44,8 @@ class Scann(BaseANN):
         self.leaves_to_search, self.reorder = leaves_reorder
 
     def query(self, v, n):
+        if self.dist == "dot_product":
+            norm = np.linalg.norm(v)
+            if norm != 0:
+                v = v / norm
         return self.searcher.search(v, n, self.reorder, self.leaves_to_search)[0]
